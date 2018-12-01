6cdf21d494886e4de93a8a0a2df77aba2a91e017
rahul.gopinath <rahul@gopinath.org>
2018-12-01 19:02:47 +0100

    udpate

--- jsontester/bin/wait.py
+++ jsontester/bin/wait.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-import sys, os, subprocess, time
+import sys, os, subprocess, time, re
 num = 2
-while num > 1:
-    snum = subprocess.check_output("pgrep -u rahul.gopinath python3 -c", shell=True)
+while num > 0:
+    #snum = subprocess.check_output("pgrep -u rahul.gopinath python3 -c", shell=True)
     result = subprocess.check_output("pgrep -u rahul.gopinath python3 -la", shell=True)
-    num = int(snum)
+    num = 0
     for i,s in enumerate(result.decode('utf-8').split("\n")):
-        print(i, s)
+        if sys.argv[1] in s and 'wait.py' not in s:
+            num += 1
+            print(i, s)
     time.sleep(10)
