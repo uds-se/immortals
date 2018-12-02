5dd7b4a66d4b65c0a2b9014bc83a9b808c9fb48a
rahul.gopinath <rahul@gopinath.org>
2018-12-02 17:25:17 +0100

    update

--- jsontester/bin/tomatrix.py
+++ jsontester/bin/tomatrix.py
@@ -9,5 +9,5 @@ lines = open(prematrix).readlines()
 for line in lines:
     mutant, *tests_ = line.split(' ')
     my_tests = set(tests_)
-    print(mutant, ','.join([('1' if t in my_tests else '0') for t in all_tests]))
+    print("%s,%s" %(mutant, ','.join([('1' if t in my_tests else '0') for t in all_tests])))
 
