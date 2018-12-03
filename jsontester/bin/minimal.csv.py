14e1f6ed6e63bbbfc7bdc0c73685e087cab4c5af
rahul.gopinath <rahul@gopinath.org>
2018-12-03 10:07:04 +0100

    update

--- jsontester/bin/minimal.csv.py
+++ jsontester/bin/minimal.csv.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
 
-lines = [l.strip().split(',') for l in sys.stdin.readlines()]
+if len(sys.argv) < 2:
+    lines = [l.strip().split(',') for l in sys.stdin.readlines()]
+else:
+    lines = [l.strip().split(',') for l in open(sys.argv[1]).readlines()]
 
 # remove head
 
