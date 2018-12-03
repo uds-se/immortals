4e475299410b5584ef3db41f021d48efc2bdc616
Rahul Gopinath <rahul@gopinath.org>
2018-12-03 08:01:16 -0800

    update

--- jsontester/bin/wait.py
+++ jsontester/bin/wait.py
@@ -3,7 +3,11 @@ import sys, os, subprocess, time, re
 num = 2
 while num > 0:
     #snum = subprocess.check_output("pgrep -u rahul.gopinath python3 -c", shell=True)
-    result = subprocess.check_output("pgrep -u rahul.gopinath python3 -la", shell=True)
+    result = b''
+    try:
+        result = subprocess.check_output("pgrep -u rahul.gopinath python3 -la", shell=True)
+    except:
+        pass
     num = 0
     for i,s in enumerate(result.decode('utf-8').split("\n")):
         if sys.argv[1] in s and 'wait.py' not in s:
