96673ab6f9004951c5330b5fd27ef453b8a797b6
Rahul Gopinath <rahul@gopinath.org>
2018-12-07 09:39:44 -0800

    update

---
+++ jsontester/tests/ijson_ctest.py
@@ -0,0 +1,207 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+import unittest
+from io import BytesIO, StringIO
+from decimal import Decimal
+import threading
+import timeout_decorator
+
+import ijson
+
+
+JSON = b'''
+{
+  "docs": [
+    {
+      "null": null,
+      "boolean": false,
+      "true": true,
+      "integer": 0,
+      "double": 0.5,
+      "exponent": 1.0e+2,
+      "long": 10000000000,
+      "string": "\\u0441\\u0442\\u0440\\u043e\\u043a\\u0430 - \xd1\x82\xd0\xb5\xd1\x81\xd1\x82"
+    },
+    {
+      "meta": [[1], {}]
+    },
+    {
+      "meta": {"key": "value"}
+    },
+    {
+      "meta": null
+    }
+  ]
+}
+'''
+JSON_EVENTS = [
+    ('start_map', None),
+        ('map_key', 'docs'),
+        ('start_array', None),
+            ('start_map', None),
+                ('map_key', 'null'),
+                ('null', None),
+                ('map_key', 'boolean'),
+                ('boolean', False),
+                ('map_key', 'true'),
+                ('boolean', True),
+                ('map_key', 'integer'),
+                ('number', 0),
+                ('map_key', 'double'),
+                ('number', Decimal('0.5')),
+                ('map_key', 'exponent'),
+                ('number', 100),
+                ('map_key', 'long'),
+                ('number', 10000000000),
+                ('map_key', 'string'),
+                ('string', 'строка - тест'),
+            ('end_map', None),
+            ('start_map', None),
+                ('map_key', 'meta'),
+                ('start_array', None),
+                    ('start_array', None),
+                        ('number', 1),
+                    ('end_array', None),
+                    ('start_map', None),
+                    ('end_map', None),
+                ('end_array', None),
+            ('end_map', None),
+            ('start_map', None),
+                ('map_key', 'meta'),
+                ('start_map', None),
+                    ('map_key', 'key'),
+                    ('string', 'value'),
+                ('end_map', None),
+            ('end_map', None),
+            ('start_map', None),
+                ('map_key', 'meta'),
+                ('null', None),
+            ('end_map', None),
+        ('end_array', None),
+    ('end_map', None),
+]
+SCALAR_JSON = b'0'
+INVALID_JSONS = [
+    b'["key", "value",]',      # trailing comma
+    b'["key"  "value"]',       # no comma
+    b'{"key": "value",}',      # trailing comma
+    b'{"key": "value" "key"}', # no comma
+    b'{"key"  "value"}',       # no colon
+    b'invalid',                # unknown lexeme
+    b'[1, 2] dangling junk'    # dangling junk
+]
+YAJL1_PASSING_INVALID = INVALID_JSONS[6]
+INCOMPLETE_JSONS = [
+    b'',
+    b'"test',
+    b'[',
+    b'[1',
+    b'[1,',
+    b'{',
+    b'{"key"',
+    b'{"key":',
+    b'{"key": "value"',
+    b'{"key": "value",',
+]
+STRINGS_JSON = br'''
+{
+    "str1": "",
+    "str2": "\"",
+    "str3": "\\",
+    "str4": "\\\\",
+    "special\t": "\b\f\n\r\t"
+}
+'''
+NUMBERS_JSON = b'[1, 1.0, 1E2]'
+SURROGATE_PAIRS_JSON = b'"\uD83D\uDCA9"'
+
+
+class Parse(unittest.TestCase):
+    '''
+    Base class for parsing tests that is used to create test cases for each
+    available backends.
+    '''
+    @timeout_decorator.timeout(10)
+    def test_basic_parse(self):
+        events = list(ijson.basic_parse(BytesIO(JSON)))
+        #self.assertEqual(events, JSON_EVENTS)
+
+    #@timeout_decorator.timeout(10)
+    #def test_basic_parse_threaded(self):
+    #    thread = threading.Thread(target=self.test_basic_parse)
+    #    thread.start()
+    #    thread.join()
+
+    @timeout_decorator.timeout(10)
+    def test_scalar(self):
+        events = list(ijson.basic_parse(BytesIO(SCALAR_JSON)))
+        #self.assertEqual(events, [('number', 0)])
+
+    @timeout_decorator.timeout(10)
+    def test_strings(self):
+        events = list(ijson.basic_parse(BytesIO(STRINGS_JSON)))
+        strings = [value for event, value in events if event == 'string']
+        #self.assertEqual(strings, ['', '"', '\\', '\\\\', '\b\f\n\r\t'])
+        #self.assertTrue(('map_key', 'special\t') in events)
+
+    @timeout_decorator.timeout(10)
+    def test_surrogate_pairs(self):
+        event = next(ijson.basic_parse(BytesIO(SURROGATE_PAIRS_JSON)))
+        parsed_string = event[1]
+        #self.assertEqual(parsed_string, '💩')
+
+    @timeout_decorator.timeout(10)
+    def test_numbers(self):
+        events = list(ijson.basic_parse(BytesIO(NUMBERS_JSON)))
+        types = [type(value) for event, value in events if event == 'number']
+        #self.assertEqual(types, [int, Decimal, Decimal])
+
+    #@timeout_decorator.timeout(10)
+    #def test_invalid(self):
+    #    for json in INVALID_JSONS:
+    #        # Yajl1 doesn't complain about additional data after the end
+    #        # of a parsed object. Skipping this test.
+    #        if self.__class__.__name__ == 'YajlParse' and json == YAJL1_PASSING_INVALID:
+    #            continue
+    #        with self.assertRaises(ijson.JSONError) as cm:
+    #            list(ijson.basic_parse(BytesIO(json)))
+
+    #@timeout_decorator.timeout(10)
+    #def test_incomplete(self):
+    #    for json in INCOMPLETE_JSONS:
+    #        with self.assertRaises(ijson.IncompleteJSONError):
+    #            list(ijson.basic_parse(BytesIO(json)))
+
+    #@timeout_decorator.timeout(10)
+    #def test_utf8_split(self):
+    #    buf_size = JSON.index(b'\xd1') + 1
+    #    try:
+    #        events = list(ijson.basic_parse(BytesIO(JSON), buf_size=buf_size))
+    #    except UnicodeDecodeError:
+    #        self.fail('UnicodeDecodeError raised')
+
+    @timeout_decorator.timeout(10)
+    def test_lazy(self):
+        # shouldn't fail since iterator is not exhausted
+        ijson.basic_parse(BytesIO(INVALID_JSONS[0]))
+        #self.assertTrue(True)
+
+    @timeout_decorator.timeout(10)
+    def test_boundary_lexeme(self):
+        buf_size = JSON.index(b'false') + 1
+        events = list(ijson.basic_parse(BytesIO(JSON), buf_size=buf_size))
+        #self.assertEqual(events, JSON_EVENTS)
+
+    @timeout_decorator.timeout(10)
+    def test_boundary_whitespace(self):
+        buf_size = JSON.index(b'   ') + 1
+        events = list(ijson.basic_parse(BytesIO(JSON), buf_size=buf_size))
+        #self.assertEqual(events, JSON_EVENTS)
+
+    #def test_api(self):
+    #   self.assertTrue(list(ijson.items(BytesIO(JSON), '')))
+    #   self.assertTrue(list(ijson.parse(BytesIO(JSON))))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ jsontester/tests/microjson_ctest.py
@@ -0,0 +1,231 @@
+import timeout_decorator
+# std
+import sys
+import unittest
+
+# vendor
+import microjson
+
+
+T_PARSE_DICTS = [
+    ('{}', {}),
+    ('{"a":1}', {"a":1}),
+    ('{"abcdef": "ghijkl"}', {'abcdef': 'ghijkl'}),
+
+    # whitespace tests
+    ('\t{\n\r\t }\r\n', {}),
+    (' \t{ "a"\n:\t"b"\n\t}  ', {"a":"b"})
+    ]
+
+T_PARSE_STRS = [
+    ('', None),
+
+    ('"foo bar baz"', 'foo bar baz'),
+    ('"abc\\"def\\"ghi"', 'abc"def"ghi'),
+
+    # escaped whitespace
+    ('"\\n\\tindent\\r\\n"', '\n\tindent\r\n'),
+
+    # escaped ascii, weird but need to test to cover possible cases
+    ('"\\ \\x\\y\\z\\ "', ' xyz '),
+
+    # escaped unicode 
+    #('"\u0124\u0113\u013a\u013e\u014d"', u"\u0124\u0113\u013a\u013e\u014d"),
+    #('"\u201chello\u201d"', u"\u201chello\u201d"),
+
+    # bare utf-8 
+    ('"\xc6\x91"', u"\u0191"),
+    ('"\xc4\x91"', u"\u0111"),
+
+    # mixed utf-8 and escaped unicode
+    #('"a\xc6\x91b\u0191c\u2018"', u"a\u0191b\u0191c\u2018"),
+    ]
+
+# utf-8 encoded ucs-4 test case
+if sys.maxunicode == 1114111:
+    T_PARSE_STRS += [
+        # pure utf-8 char > 16-bits. not asci-encodable in json.
+        ('"\xf0\x90\x82\x82"', U"\U00010082")
+    ]
+
+# test range of 16-bit characters > 0x7F
+
+T_PARSE_UNICODE = [('"%s"' % c, c) for c in 
+        (chr(i) for i in range(0x80, 0x10000))
+    ]
+
+T_PARSE_LISTS = [
+    ('[]', []),
+    ('[1,2,3]', [1,2,3]),
+    ('[[1,2],["a","b"]]', [[1,2],["a","b"]]),
+
+    # whitespace tests
+    ('\t\n[\r\n \t]\n', []),
+    ('  [\n\t1,\t2 ] \t', [1,2])
+
+    ]
+
+T_PARSE_INTS = [
+    ('0', 0),
+    ('-1', -1),
+    ('123', 123), 
+    ('-2147483648', -2147483648),
+    ('2147483648', 2147483648),
+    ('4294967296', 4294967296),
+    ('9223372036854775808', 9223372036854775808),
+    ('18446744073709551616', 18446744073709551616)
+    ]
+
+T_PARSE_FLOATS = [
+    ('.1', 0.1),
+    ('-.1', -0.1),
+    ('1.0', 1.0),
+    ('-1.0', -1.0),
+    ('3.14159', 3.14159),
+    ('-3.14159', -3.14159),
+    ('1E1', 1E1),
+    ('-1E2', -1E2),
+    ('-1E-2', -1E-2),
+    ('12E-2', 12E-2),
+    ('1.8446744073709552e19', 1.8446744073709552e19)
+    ]
+
+T_PARSE_FIXED = [('true', True), ('false', False), ('null', None)]
+
+T_PARSE_MALFORMED = [
+    'wegouhweg',    # naked char data
+    '["abcdef]',    # string missing trailing '"'
+    '["a","b"',     # list missing trailing ']'
+    '{"a:"b"}',     # key missing trailing '"'
+    '{"a":13',      # dict missing trailing '}'
+    '{123: 456}',   # object keys must be quoted
+    '[nulx]',       # null?
+    '[trux]',       # true?
+    "[12, ]",       # incomplete list
+    "[123",         # truncated list
+    "[1, , ,]",     # list with empty slots
+    "[1, , ",       # truncated list with empty slots
+    "[#",           # list with illegal chars
+    "[1, 2\n#",     # list with illegal chars
+    '{"abc"}',      # incomplete dict
+    '{"abc"',       # truncated dict
+    '{"abc":',      # truncated dict with missing value
+    '{',            # truncated dict
+    '{,}',          # dict with empty slots
+    #u'[]',          # input must be a str
+    #'{"a":1"b":2}'  # missing comma
+    ]
+
+
+
+class TestMicrojsonParse(unittest.TestCase):
+
+    def _run_cases(self, cases):
+        for js, py in cases:
+            r = microjson.from_json(js)
+            #self.assertEqual(r, py)
+
+    @timeout_decorator.timeout(10)
+    def test_dict(self):
+        self._run_cases(T_PARSE_DICTS)
+
+    @timeout_decorator.timeout(10)
+    def test_list(self):
+        self._run_cases(T_PARSE_LISTS)
+
+    @timeout_decorator.timeout(10)
+    def test_string(self):
+        self._run_cases(T_PARSE_STRS)
+
+    #def test_unicode(self):
+    #    self._run_cases(T_PARSE_UNICODE)
+
+    @timeout_decorator.timeout(10)
+    def test_integer(self):
+        self._run_cases(T_PARSE_INTS)
+    
+    @timeout_decorator.timeout(10)
+    def test_floats(self):
+        self._run_cases(T_PARSE_FLOATS)
+
+    @timeout_decorator.timeout(10)
+    def test_null_and_bool(self):
+        self._run_cases(T_PARSE_FIXED)
+
+    #@timeout_decorator.timeout(10)
+    #def test_malformed(self):
+    #    "assert a JSONError is raised for these cases"
+    #    for js in T_PARSE_MALFORMED:
+    #        self.assertRaises(microjson.JSONError, microjson.from_json, js)
+
+#    @timeout_decorator.timeout(10)
+#    def test_aliases(self):
+#        data = "abc"
+#        json = '"abc"'
+#        r = microjson.decode(json)
+#        self.assertEqual(r, data)
+#        r = microjson.encode(data)
+#        self.assertEqual(r, json)
+
+
+#T_EMIT_VALID = [
+#    #(u"\"\n\t\u2018hi\u2019", '"\\\"\\n\\t\u2018hi\u2019"'),
+#    #(u"se\u00f1or", '"se\u00f1or"'),
+#    (["foo", "bar"], '["foo","bar"]'),
+#    (3.14159, "3.14159"),
+#    (10e20, "1e+21"),
+#    (-100, "-100"),
+#    (0x351, "849"),
+#    ({"a":1,"b":[1,2,3]}, '{"a":1,"b":[1,2,3]}'),
+#    ((None, True, False), "[null,true,false]"),
+#
+#    # non-string keys are cast to str
+#    ({1: 2}, '{"1":2}'),
+#
+#    ]
+#
+#T_EMIT_INVALID = [
+#    float('inf'),
+#    float('-inf'),
+#    float('nan'),
+#    ]
+#
+#
+#class TestMicrojsonEmit(unittest.TestCase):
+#
+#    def _run_cases(self, cases):
+#        for py, js in cases:
+#            r = microjson.to_json(py)
+#            self.assertEqual(r, js)
+#
+#    @timeout_decorator.timeout(10)
+#    def test_valid(self):
+#        self._run_cases(T_EMIT_VALID)
+#
+#    @timeout_decorator.timeout(10)
+#    def test_invalid(self):
+#        for py in T_EMIT_INVALID:
+#            self.assertRaises(microjson.JSONError, microjson.to_json, py)
+#
+#    @timeout_decorator.timeout(10)
+#    def test_string_object(self):
+#        class Bag:
+#            pass
+#        cases = [
+#            ("hello", '"hello"'),
+#            (u"\u2018hi\u2019", r'"\u2018hi\u2019"')
+#            ]
+#        for py, js in cases:
+#            for meth in ('__str__', '__unicode__'):
+#                obj = Bag()
+#                setattr(obj, meth, lambda: py)
+#                r = microjson.to_json(obj)
+#                self.assertEqual(r, js)
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
+        
