2c4a35db77cc5bce05bae4b5dc9a5e77d055f0e2
Rahul Gopinath <rahul@gopinath.org>
2019-06-13 02:37:43 -0700

    update

--- jsontester/bin/gfuzz.py
+++ jsontester/bin/gfuzz.py
@@ -2,6 +2,7 @@
 # This is a grammar fuzzer
 
 import random
+import string
 random.seed(0)
 import fuzzingbook.GrammarFuzzer as GF
 import fuzzingbook.GrammarCoverageFuzzer as GCF
@@ -25,7 +26,7 @@ J_G = {
             'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
             '!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.',
             '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`',
-            '{', '|', '}', '~', ' '],
+            '{', '|', '}', '~', ' ', '\\"', '\\\\', '\\/', '<unicode>', '<escaped>'],
         '<number>': ['<int><frac><exp>'],
         '<int>': ['<digit>', '<onenine><digits>', '-<digits>', '-<onenine><digits>'],
         '<digits>': ['<digit-1>'],
@@ -34,19 +35,29 @@ J_G = {
         '<frac>': ['', '.<digits>'],
         '<exp>': ['', 'E<sign><digits>', 'e<sign><digits>'],
         '<sign>': ['', '+', '-'],
-        '<ws>': [' <ws>', ''],
+        '<ws>': ['<sp1><ws>', ''],
+        '<sp1>': ['\n', '\r', '\t', '\b', '\f'],
         '<symbol>': [',<members>'],
         '<symbol-1>': [',<elements>'],
         '<symbol-2>': ['', '<symbol><symbol-2>'],
         '<symbol-1-1>': ['', '<symbol-1><symbol-1-1>'],
         '<character-1>': ['', '<character><character-1>'],
-        '<digit-1>': ['<digit>', '<digit><digit-1>']
+        '<digit-1>': ['<digit>', '<digit><digit-1>'],
+        '<unicode>': [chr(i) for i in range(0x1F, 0x10000) if chr(i) not in string.printable],
+        '<escaped>': ['\\u<hex><hex><hex><hex>'],
+        '<hex>': [str(i) for i in range(10)] + list('abcdefABCDEF')
         }
 
 
 import string
 import json
 import sys
+sys.setrecursionlimit(1500)
+#import resource
+# Will segfault without this line.
+#resource.setrlimit(resource.RLIMIT_STACK, [0x10000000, resource.RLIM_INFINITY])
+#sys.setrecursionlimit(0x100000)
+
 from importlib import import_module
 
 import sys, os, importlib
@@ -70,14 +81,15 @@ def dump_and_flush(f, s):
     f.flush()
 
 import pickle
-Max = 10 # takes 18 minutes for 10000
+Max = int(os.environ.get('GFUZZ_MAX', '1000')) # takes 18 minutes for 10000
+print("Max: ", Max)
 import time
 start_time = time.monotonic()
 percentage_points = {i*100 for i in range(1,Max//100)}
 with open('%s.gfuzz.pickled' % mfile, 'wb+') as f:
     for i in range(Max):
         if i in percentage_points:
-            print(i/Max, time.monotonic() - start_time)
+            print(i/Max, time.monotonic() - start_time, file=sys.stderr)
         sys.stdout.flush()
         s = json_fuzzer.fuzz() 
         sys.stdout.flush()
