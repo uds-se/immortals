f00accb123154ae11b4b0997b69e52a4aab05a40
Rahul Gopinath <rahul@gopinath.org>
2019-07-04 12:51:33 -0700

    update

--- jsontester/bin/wait.py
+++ jsontester/bin/wait.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import sys, os, subprocess, time, re
+import sys, os, subprocess, time, re, signal
 num = 2
+seen = {}
 while num > 0:
     #snum = subprocess.check_output("pgrep -u rahul.gopinath python3 -c", shell=True)
     result = b''
@@ -13,5 +14,11 @@ while num > 0:
         if sys.argv[1] in s and 'wait.py' not in s:
             num += 1
             print(i, s)
+            if i not in seen:
+                seen[i] = 0
+            else:
+                seen[i] += 1
+            if seen[i] > 10:
+                os.kill(i, signal.SIGALRM)
     time.sleep(10)
     print()
